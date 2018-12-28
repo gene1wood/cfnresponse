977799a5c14e8293365311a7fe32f5c6ac0a9003
Gene Wood <gene_wood@cementhorizon.com>
2018-12-28 14:06:44 -0800

    Fix README typo

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="cfnresponse",
-    version="1.0.0",
+    version="1.0.1",
     author="Amazon Web Services",
     description="Send a response object to a custom resource by way of an Amazon S3 presigned URL",
     long_description=long_description,
